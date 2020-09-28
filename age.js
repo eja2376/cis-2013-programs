68a2f93e7483b061816288cd8a2ede8da1cd44d8
eja2376 <70679087+eja2376@users.noreply.github.com>
2020-09-28 13:44:23 -0500

    Update age.js

--- age.js
+++ age.js
@@ -1,7 +1,7 @@
 var floatAge, floatDays, intWeeks, floatMonths, intFortnights;
     floatAge = parseFloat(prompt("Please enter your age:"));
-    floatDays = parseFloat(floatAge * 365.25);
-    floatMonths = parseFloat(floatAge * 12);
-    intWeeks = parseInt(floatDays / 7);
-    intFortnights = parseInt(floatDays / 14);
+    floatDays = parseFloat(floatAge * 365.25).toFixed(2);
+    floatMonths = parseFloat(floatAge * 12).toFixed(2);
+    intWeeks = parseInt(floatDays / 7).toFixed(2);
+    intFortnights = parseInt(floatDays / 14).toFixed(2);
     alert("Age entered: " + floatAge + " Your age in days: " + floatDays + " Your age in months: " + floatMonths + " Your age in weeks: " + intWeeks + " Your age in fortnights: " + intFortnights);
